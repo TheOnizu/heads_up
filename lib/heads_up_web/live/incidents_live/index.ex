9926cada20bdc9cc7003421cbc3ccfb4e90cbf29
theonizuh <gaoussoukone@protonmail.com>
2025-01-15 15:55:55 +0100

    add custom optional attributes

--- lib/heads_up_web/live/incidents_live/index.ex
+++ lib/heads_up_web/live/incidents_live/index.ex
@@ -18,9 +18,16 @@ defmodule HeadsUpWeb.IncidentsLive.Index do
   end
 
   attr :status, :atom, values: [:pending, :resolved, :canceled], default: :pending
+  attr :class, :string, default: nil
   def badge(assigns) do
     ~H"""
-    <div class="rounded-md px-2 py-1 text-xs font-medium uppercase inline-block border text-lime-600 border-lime-600">
+    <div class={[
+      "rounded-md px-2 py-1 text-xs font-medium uppercase inline-block border",
+      @status == :pending && "text-lime-600 border-lime-600",
+      @status == :resolved && "text-amber-600 border-amber-600",
+      @status == :canceled && "text-red-600 border-red-600",
+      @class
+    ]}>
       { @status }
     </div>
     """
@@ -34,7 +41,7 @@ defmodule HeadsUpWeb.IncidentsLive.Index do
       <img src={@incident.image_path} />
       <h2>{ @incident.name }</h2>
       <div class="details">
-        <.badge status={@incident.status} />
+        <.badge status={@incident.status} class="animate-spin" />
         <div class="priority">
           { @incident.priority }
         </div>
