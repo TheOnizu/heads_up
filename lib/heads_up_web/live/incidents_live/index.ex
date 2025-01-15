5bee07a84f016edfe6293788ecfecc3a880dfaa4
theonizuh <gaoussoukone@protonmail.com>
2025-01-15 13:24:39 +0100

    function components

--- lib/heads_up_web/live/incidents_live/index.ex
+++ lib/heads_up_web/live/incidents_live/index.ex
@@ -11,17 +11,32 @@ defmodule HeadsUpWeb.IncidentsLive.Index do
     ~H"""
     <div class="incident-index">
       <div class="incidents" >
-        <div class="card" :for={incident <- @incidents}>
-          <img src={incident.image_path} />
-          <h2>{ incident.name }</h2>
-          <div class="details">
-            <div class="badge">
-              { incident.status }
-            </div>
-            <div class="priority">
-              { incident.priority }
-            </div>
-          </div>
+        <.incident_card :for={incident <- @incidents} incident={incident} />
+      </div>
+    </div>
+    """
+  end
+
+  attr :status, :atom, values: [:pending, :resolved, :canceled], default: :pending
+  def badge(assigns) do
+    ~H"""
+    <div class="rounded-md px-2 py-1 text-xs font-medium uppercase inline-block border text-lime-600 border-lime-600">
+      { @status }
+    </div>
+    """
+  end
+
+  attr :incident, HeadsUp.Incident, required: true
+
+  def incident_card(assigns) do
+    ~H"""
+    <div class="card">
+      <img src={@incident.image_path} />
+      <h2>{ @incident.name }</h2>
+      <div class="details">
+        <.badge status={@incident.status} />
+        <div class="priority">
+          { @incident.priority }
         </div>
       </div>
     </div>
